fd6abb3a0185942ee6fa4854ec35b3b5f9e20412
tomBperry <tom.perry0@yahoo.co.uk>
2018-06-29 19:16:35 +0100

    Fully working except for the energy

--- Modes/sketch.js
+++ Modes/sketch.js
@@ -5,36 +5,38 @@ let Amp, energy;
 let rowSep, c;
 let Restoringforce;
 
-const radius = 1;
-const trailLength = 5;
+const radius = 0.2;
 
 const T = 10;
 const damping = 1;
 
-const N = 300;
-const M = 1;
+const N = 500; // 500 is the optimal number on the 29/06/18
+const M = 2; // Number of Modes
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(766, 455);
   colorMode(RGB, 255);
 
   rowSep = width / (N + 1);
   c = T / width;
-  const A = 200/M;
+  const A = 0.5 * height / M;
 
   zero = createVector(0, 0);
   unit = createVector(0, 1);
+  endPoint = createVector(width, height / 2);
 
   for (let n = 0; n < N; n++) {
     balls.push(new Ball(n));
 
-    for (let i = 1; i <= M; i++) {
+    balls[n].minSep = rowSep;
+    balls[n].maxSep = rowSep;
+
+    for (let i = 0; i <= M; i++) {
       balls[n].position.y = balls[n].position.y +
-        A * sin(2*i * PI * balls[n].position.x / width);
+        A * sin(i * PI * balls[n].position.x / width);
     }
   }
 }
-
 
 
 function draw() {
@@ -47,20 +49,29 @@ function draw() {
   for (let n = 0; n < balls.length; n++) {
     calcForce(n);
     balls[n].addForce(unit.copy().mult(Fn));
-  }
 
-  for (let n = 0; n < balls.length; n++) {
+    // Gravity
+    // balls[n].addForce(unit.copy().mult(0.03));
+
     balls[n].update();
+    // realtiveSep(n);
     balls[n].show();
-    balls[n].trails();
-    energy += balls[n].Energy();
+    // energy += balls[n].Energy();
   }
-  if (frameCount % 60 == 0) {
-    console.log("Energy: " + energy);
-  }
-  energy = 0;
+  // energy += T * (sqrt(balls[0].position.x * balls[0].position.x +
+  //   (balls[0].position.y-width/2) * (balls[0].position.y-width/2))-rowSep);
+  //
+  // if (frameCount % 6 == 0) {
+  //   console.log("Energy: " + energy);
+  // }
+  // energy = 0;
 
-  // realtiveSep();
-  showSprings();
+  // showSprings();
 
+  //  To Greatly improve performance, remove the showSprings and realtiveSep
+  // functions.
+
+  // To have energy showing, the relativeSep function must be on
+
+  // Energy not working
 }

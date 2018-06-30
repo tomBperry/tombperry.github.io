d380c4b4c6023c6708cf82d02842a3e260e12dea
tomBperry <tom.perry0@yahoo.co.uk>
2018-06-30 09:52:17 +0100

    Working example of an angle worked lumpy string

--- Modes/mass.js
+++ Modes/mass.js
@@ -1,32 +1,24 @@
 let springColour, springThickness;
-let sep;
-let minSep = 100;
-let maxSep = 100;
-let dx, dy, theta;
-
+let dy, theta;
 
 class Ball {
-  constructor(n, radii = radius, vx0 = 0, vy0 = 0, colour = 255, trail = false) {
-    this.position = createVector((n + 1) * rowSep, height / 2);
-    this.eqPos = this.position;
-    this.disp = this.position.copy().sub(this.eqPos);
+  constructor(n, radii = radius, colour = 255) {
     this.angle = 0
-    this.maxAmp = 0;
-    this.velocity = createVector(vx0, vy0);
+
+    this.position = createVector((n + 1) * rowSep, height / 2);
+    this.velocity = zero.copy();
     this.acceleration = zero.copy();
+
     this.colour = colour;
     this.radius = radii;
     this.mass = PI * this.radius * this.radius;
-    if (trail) {
-      this.trail = [];
-    }
   }
 
   update() {
-    this.velocity.add(this.acceleration);
+    this.velocity.add(this.acceleration.copy());
     this.position.add(this.velocity.copy());
-    this.velocity.mult(damping);
-    this.acceleration.mult(0);
+    // this.velocity.mult(damping);
+    this.acceleration.set(zero);
   }
 
   addForce(force) {
@@ -35,84 +27,18 @@ class Ball {
 
   show() {
     fill(this.colour);
-    stroke(this.colour, 150);
-    strokeWeight(2);
+    noStroke();
+    // stroke(this.colour, 150);
+    // strokeWeight(this.radius);
     ellipse(this.position.x, this.position.y, 2 * this.radius);
-  }
-
-  trails() {
-    this.trail.push(this.position.x, this.position.y);
-    if (this.trail.length > 2 * trailLength) {
-      this.trail.splice(0, 2);
-    }
-
-    stroke(this.colour);
-    strokeWeight(0.2);
-    noFill();
-    beginShape();
-    for (let i = 0; i < this.trail.length; i += 2) {
-      vertex(this.trail[i], this.trail[i + 1]);
-    }
-    endShape();
-  }
-
-  kEnergy() {
-    return 0.5 * this.mass * this.velocity.dot(this.velocity);
-  }
-
-  amplitude() {
-    this.disp.set(this.position.copy().sub(this.eqPos));
-    if (this.disp.mag() > this.maxAmp) {
-      this.maxAmp = this.disp.mag();
-      return this.maxAmp;
-    }
-  }
-}
-
-function distanceSq(a, b) {
-  let dx = a.position.x - b.position.x;
-  let dy = a.position.y - b.position.y;
-  return dx * dx + dy * dy;
-}
-
-function showSprings() {
-  springThickness = map(abs((balls[0].position.copy().sub(balls[1].position)).mag()),
-    minSep, maxSep, 10, 5);
-  springColour = map(abs((balls[0].position.copy().sub(balls[1].position)).mag()),
-    minSep, maxSep, 0, 100);
-  strokeWeight(springThickness);
-  stroke(100, springColour, 200, 200);
-  line(balls[0].position.x, balls[0].position.y, balls[1].position.x,
-    balls[1].position.y);
-
-  for (i = 0; i < balls.length; i++) {
-    springThickness = map(abs(balls[i].disp.mag()), 0, balls[i].maxAmp, 10, 5);
-    springColour = map(abs(balls[i].disp.mag()), 0, balls[i].maxAmp, 100, 255);
-    stroke(springColour, 0, 0, 150);
-    strokeWeight(springThickness);
-    line(0 + i * width, balls[i].eqPos.y,
-      balls[i].position.x, balls[i].position.y);
-  }
-}
-
-function realtiveSep() {
-  sep = abs((balls[0].position.copy().sub(balls[1].position.copy())).mag());
-
-  if (sep > maxSep) {
-    maxSep = sep;
-  }
-  if (sep < minSep) {
-    minSep = sep;
   }
 }
 
 function calcAngle(n) {
-  if(n == N - 1) {
-    dx = width - balls[n].position.x;
-    dy = height/2 - balls[n].position.y;
+  if (n == N - 1) {
+    dy = height / 2 - balls[n].position.y;
   } else {
-    dx = balls[n + 1].position.x - balls[n].position.x;
     dy = balls[n + 1].position.y - balls[n].position.y;
   }
-  return theta = atan(dy / dx);
+  return theta = atan(dy / rowSep);
 }
--- Modes/sketch.js
+++ Modes/sketch.js
@@ -1,77 +1,53 @@
 let balls = [];
-let zero, unitx, unity;
-let k = [];
-let c = [];
-let Amp, energy;
+let zero, unit;
 let rowSep;
+let Fn, f0;
+
 const radius = 3;
-const trailLength = 200;
-let Fxn, Fyn, Fx0, Fy0;
-
-const T = 20;
+const T = 3;
 const damping = 1;
+const N = 500;
+const M = 2;
 
-const N = 50;
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(770, 450);
   colorMode(RGB, 255);
-
+  const A = 0.5 * height / M
   rowSep = width / (N + 1);;
-
-  zero = createVector(0, 0);
-  unitx = createVector(1, 0);
-  unity = createVector(0, 1);
+  zero = createVector();
+  unit = createVector(0, 1);
 
   for (let n = 0; n < N; n++) {
     balls.push(new Ball(n));
-  }
-  balls[25].position.add(0, 10);
 
-  balls[0].colour = color(255, 0, 0);
-  balls[N-1].colour = color(255, 0, 0);
-  // balls[2].position.add(0, -50);
-  // balls[0].position.add(0, 40);
+    for (let i = 0; i <= M; i++) {
+      balls[n].position.y = balls[n].position.y +
+        A * sin(i * PI * balls[n].position.x / width);
+    }
+  }
+
 }
 
 function draw() {
   background(0);
 
-  for (let n = 1; n < balls.length; n++) {
-    balls[n].update();
+  for (let n = 0; n < balls.length; n++) {
     balls[n].angle = calcAngle(n);
-    //Amp = balls[n].amplitude();
   }
+  dy = balls[0].position.y - (height / 2);
+  theta0 = atan(dy / rowSep);
 
+  f0 = T * (sin(balls[0].angle) - sin(theta0)); //T * (balls[0].angle - theta0);
+  balls[0].addForce(unit.copy().mult(f0));
 
-  dx = balls[0].position.x;
-  dy = (height/2)-balls[0].position.y;
-  theta0 = atan(dy/dx);
-
-  Fy0 = T * (sin(balls[0].angle)-sin(theta0));
-  // Fx0 = T * (cos(balls[0].angle)-cos(theta0));
-
-  // balls[0].addForce(unitx.copy().mult(Fxn));
-  //balls[0].addForce(unity.copy().mult(Fyn));
-
-  for (let n = 1; n < balls.length-1; n++) {
-    Fyn = T * (sin(balls[n].angle) - sin(balls[n - 1].angle));
-    // Fxn = T * (cos(balls[n].angle) - cos(balls[n - 1].angle))
-
-    // balls[n].addForce(unitx.copy().mult(Fxn));
-    balls[n].addForce(unity.copy().mult(Fyn));
+  for (let n = 1; n < balls.length; n++) {
+    Fn = T * (sin(balls[n].angle) - sin(balls[n - 1].angle));// T * (balls[n].angle - balls[n - 1].angle);
+    balls[n].addForce(unit.copy().mult(Fn));
   }
 
   for (let n = 0; n < balls.length; n++) {
-    // balls[n].update();
+    balls[n].update();
     balls[n].show();
-    // balls[n].trails();
-    energy += balls[n].kEnergy();
   }
-  // realtiveSep();
-  // showSprings();
-
-  console.log(energy);
-  energy = 0;
-
 }
